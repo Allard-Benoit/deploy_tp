71ffaf464ac016851305ff6efe56ddd908fa3cb0
Allard-Benoit <91566255+Allard-Benoit@users.noreply.github.com>
2023-01-13 00:03:23 +0100

    Add files via upload

---
+++ deploy/README.md
@@ -0,0 +1,5 @@
+# met02 / Benoît
+
+Architecture pour deployer app spa angular/php sur render.com
+
+slim/jwt/doctrine
---
+++ deploy/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+date_default_timezone_set('America/Lima');
+require_once "vendor/autoload.php";
+$isDevMode = true;
+$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+$conn = array(
+'host' => 'dpg-ceguqu82i3mkhvqh5740-a',
+'driver' => 'pdo_pgsql',
+'user' => 'met02_allard_benoit_user',
+'password' => 'I5cKzQtbZTfc7Iw7vzRMEDA2f6ZDOb5k',
+'dbname' => 'met02_allard_benoit',
+'port' => '5432'
+);
+$entityManager = EntityManager::create($conn, $config);
